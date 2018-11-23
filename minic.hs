a1f2b9b810df0c33b1d154553a5c9559ee65efd0
Park, Sanguk <noorim.ailen@gmail.com>
2018-11-23 18:35:46 +0900

    Initial commit: including specification of assignment and implemented parser

---
+++ minic.hs
@@ -0,0 +1,175 @@
+-- mini-C compiler
+
+module MyCompiler where
+
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Language
+import Text.Parsec.Token
+import Text.Parsec.Expr
+import Control.Applicative hiding ((<|>), many)
+
+--------------------------------------------------------------
+-- Lexer
+
+lang = emptyDef {identStart=letter <|> char '_',
+                 identLetter=alphaNum <|> char '_',
+                 reservedNames=["int", "float", "for", "if"]}
+
+lexer = makeTokenParser lang
+
+paren = parens lexer
+brace = braces lexer
+bracket = brackets lexer
+number = natural lexer
+ident = identifier lexer
+literal = naturalOrFloat lexer
+keyword = reserved lexer
+op = reservedOp lexer
+
+--------------------------------------------------------------
+-- Parser
+
+data Tsmpl = Tint | Tfloat deriving (Show)
+data Type = Tsmpl Tsmpl | Tarray Tsmpl Integer | Tpointer Tsmpl deriving (Show)
+
+data Fun = Fun {
+  name :: String,
+  arguments :: [(Type, String)],
+  ret_type :: Type,
+  fun_body :: [Stmt]
+} deriving (Show)
+
+data Stmt = For {
+  init :: Maybe Expr,
+  flag :: Maybe Expr,
+  rept :: Maybe Expr,
+  body :: [Stmt]
+} | If {
+  cond :: Expr,
+  body :: [Stmt]
+} | VarDecl {
+  t_smpl :: Tsmpl,
+  names :: [DecoratedVarName]
+} | Return {
+  expr :: Expr
+} | Expr {
+  expr :: Expr
+} | Mpty deriving (Show)
+
+data Expr = Var String | LitI Integer | LitF Double | Add Expr Expr | Sub Expr Expr
+                  | Mul Expr Expr | Div Expr Expr | Inc Expr
+                  | Deref Expr | Index Expr Expr | Assgn Expr Expr
+                  | Lt Expr Expr | Gt Expr Expr | Call String [Expr] deriving (Show)
+data DecoratedVarName = Smpl String | Pointer String | Array String Integer deriving (Show)
+
+p_expr = (flip buildExpressionParser) p_term [
+  [Postfix (op "++" >> return Inc)],
+  [Prefix (op "*" >> return Deref)],
+  {-- very clever idea from
+    https://stackoverflow.com/questions/17287578/how-to-implement-subscript-operators-in-parsec
+    see comments of OP's question --}
+  [Postfix $ (flip Index) <$> (bracket p_expr)],
+  [
+    Infix (op "*" >> return Mul) AssocLeft,
+    Infix (op "/" >> return Div) AssocLeft
+  ],
+  [
+    Infix (op "+" >> return Add) AssocLeft,
+    Infix (op "-" >> return Sub) AssocLeft
+  ],
+  [
+    Infix (op "<" >> return Lt) AssocLeft,
+    Infix (op ">" >> return Gt) AssocLeft
+  ],
+  [Infix (op "=" >> return Assgn) AssocRight]]
+
+p_literal = do
+  lit <- literal
+  case lit of
+    Left i -> return $ LitI i
+    Right d -> return $ LitF d
+
+p_call = Call <$> ident <*> paren (p_expr `sepBy` op ",")
+
+p_term = p_literal <|> paren (p_expr) <|> try (p_call) <|> Var <$> ident
+
+(<&>) a b = (,) <$> a <*> b
+
+p_for_conds = optionMaybe p_expr <&> (op ";" *> optionMaybe p_expr) <&>
+                  (op ";" *> optionMaybe p_expr)
+
+p_for = do
+  keyword "for"
+  ((init, cond), rept) <- paren (p_for_conds)
+  body <- p_stmts <|> (:[]) <$> p_stmt
+  return $ For init cond rept body
+
+p_if = keyword "if" *> (If <$> paren (p_expr) <*> (p_stmts <|> (:[]) <$> p_stmt))
+
+p_deco_name = op "*" *> (Pointer <$> ident) 
+            <|> try (Array <$> ident <*> bracket(number))
+            <|> Smpl <$> ident
+
+p_var_decl = VarDecl <$> p_type_smpl <*> (p_deco_name `sepBy` op ",")
+            <* op ";"
+
+
+p_return = keyword "return" *> (Return <$> p_expr <* op ";")
+p_empty_stmt = do op ";" ; return Mpty
+
+p_stmt = p_for <|> p_if <|> p_var_decl 
+               <|> p_return <|> p_empty_stmt <|> Expr <$> p_expr <* op ";"
+p_stmts = brace (many p_stmt)
+
+p_type_smpl = keyword "int" *> return Tint <|> keyword "float" *> return Tfloat
+p_func_ret_t = try (Tpointer <$> p_type_smpl <* op "*") <|> Tsmpl <$> p_type_smpl
+p_func_arg = (,) <$> p_func_ret_t <*> ident
+          <|> do
+            t <- p_type_smpl
+            name <- ident
+            len <- bracket (number)
+            return (Tarray t len, name)
+
+p_func_args = paren (keyword "void" *> (return []) <|> p_func_arg `sepBy` op ",")
+
+p_func = do
+  ret_t <- p_func_ret_t
+  name <- ident
+  args <- p_func_args
+  body <- p_stmts
+  return $ Fun name args ret_t body
+
+p_program = many p_func
+
+parseProgram = parse p_program ""
+--------------------------------------------------------------
+
+ex_program = "int avg(int count, int *value) {\n\
+\  int i, total;;\n\
+\  for (i = 1; i < count; i++) {\n\
+\    total = toal + value[i];\n\
+\  }\n\
+\\n\
+\  return (total / count);\n\
+\}\n\
+\\n\
+\int main(void) {\n\
+\  int studentNumber, count, i, sum;\n\
+\  int mark[4];\n\
+\  float average;\n\
+\\n\
+\  count = 4;\n\
+\  sum = 0;\n\
+\\n\
+\  for (i = 0; i < count; i++) {\n\
+\    mark[i] = i * 30;\n\
+\    sum = sum + mark[i];\n\
+\    average = avg(i + 1, mark);\n\
+\    if (average > 40) {\n\
+\      printf(f, average);\n\
+\    }\n\
+\  }\n\
+\}"
+
+ex_p2 = "int avg(int count, int *value) {}"
